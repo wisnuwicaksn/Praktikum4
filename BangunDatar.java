3ddebdd1883e61ef5c87e6ac319a086ac21b241f
wisnuwicaksn <adiwisnu123@gmail.com>
2022-11-10 15:53:23 +0700

    bangun datar

---
+++ BangunDatar.java
@@ -0,0 +1,11 @@
+public class BangunDatar{
+    public float luas(){
+        System.out.println("Menghitung luas bangun datar");
+        return 0;
+    }
+
+    public float keliling(){
+        System.out.println("Menghitung keliling bangun datar");
+        return 0;
+    }
+}
